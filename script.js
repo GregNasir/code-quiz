a41d820b9d999d35daf45576cab6fa0d9140be62
GregNasir <63434657+GregNasir@users.noreply.github.com>
2023-04-25 01:37:24 -0400

    added timer and message

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 // Selects element by class
 const timeEl = document.querySelector(".time");
+const start = document.querySelector('#start');
 
 // Selects element by id
 const mainEl = document.getElementById("main-time");
 
 let secondsLeft = 60;
+
+// start.addEventListener('click', function() {
+// 	// let i = 0;
+
+//   timeEl.textContent = "Time: " + secondsLeft;
+
+
+
+//   secondsLeft--;
+	
+// 	let timerId = setInterval(function() {
+// 		console.log('!')
+// 	}, 1000);
+// });
 
 function setTime() {
   // Sets interval in variable
@@ -15,11 +30,20 @@ function setTime() {
     if (secondsLeft === 0) {
       // Stops execution of action at set interval
       clearInterval(timerInterval);
+      const countdown = document.getElementById("countdown").innerHTML = "Game Over!";
+      return countdown;
       // Calls function to create and append image
-      sendMessage();
+      // sendMessage();
     }
 
   }, 1000);
+}
+
+function startQuiz(event) {
+   event.stopPropagation();
+  event.currentTarget.setAttribute(
+    "button"
+  );
 }
 
 // // Function to create and append colorsplosion image
@@ -31,4 +55,8 @@ function setTime() {
 
 // }
 
-setTime();
+// setTime();
+
+start.addEventListener('click', setTime());
+
+// button.addEventListener("click", setTime());

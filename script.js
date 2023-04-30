8bab3dc32a50470be3638c0e5fa8b57f3ea02b1a
GregNasir <63434657+GregNasir@users.noreply.github.com>
2023-04-29 20:35:26 -0400

    added buttons to submit answers

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Selects element by class
 var startBtn = document.querySelector("#start");
+var submitBtn =document.querySelector("#submit-btn");
 var timeEl = document.querySelector(".time");
 var intro = document.querySelector("#intro");
 
@@ -14,7 +15,12 @@ var answerBtn2 = document.querySelector("#answer_btn2");
 var answerBtn3 = document.querySelector("#answer_btn3");
 var answerBtn4 = document.querySelector("#answer_btn4");
 
-var rightWrong = document.querySelector("#right-wrong");
+var checkSelection = document.querySelector("#check-selection");
+var scoreTally = document.querySelector("#submit-page")
+var scoreTotal = document.querySelector("#score-total");
+var userInitials =document.querySelector("#initials");
+
+
 
 var questionHub = [
   {
@@ -44,7 +50,12 @@ var questionHub = [
   }
 ];
 
+var timeLeft = document.getElementById("time");
+
+// var secondsLeft = 60;
 var questionNumber = 0;
+// var totalScore = 0;
+var questionCount = 1;
 
 
 // Selects element by id
@@ -79,8 +90,8 @@ function startQuiz() {
   intro.style.display = "none";
   questionsPage.style.display = "block";
   clock();
-  questionNumber = 0;
-  questions(questionNumber);
+  questionOption= 0;
+  questions(questionOption);
 
 }
 
@@ -97,37 +108,53 @@ function questions(n) {
 function answerVeri(e) {
   e.preventDefault();
   //make it display
-  rightWrong.style.display = "block";
+  checkSelection .style.display = "block";
   setTimeout(function () {
-      rightWrong.style.display = 'none';
+    checkSelection .style.display = 'none';
   }, 1000);
 
   // answer check
-  if (questionHub[questionNumber].answer == e.target.value) {
-      rightWrong.textContent = "Correct!"; 
+  if (questionHub[questionOption].answer == e.target.value) {
+    checkSelection.textContent = "Correct!"; 
       totalScore = totalScore + 1;
+      questions(questionOption +1);
 
   } else {
       secondsLeft = secondsLeft - 10;
-      rightWrong.textContent = "Wrong! The correct answer is " + questionHub[questionNumber].answer + " .";
+      checkSelection.textContent = "Wrong! The correct answer is " + questionHub[questionOption].answer + " .";
   }
        //THEN I am presented with another question
-  if (questionNumber < questionHub.length -1 ) {
+  if (questionOption < questionHub.length -1 ) {
   // call questionshub to bring in next question when any reactBtn is clicked
-      questions(questionNumber +1);
+      questions(questionOption +1);
   } else {
   gameOver();
 }
 questionCount++;
 }
 
-
-
-
-
 function gameOver() {
-  return gameOver();
-}
+
+  questionPage.style.display = "none";
+  scoreTally.style.display = "block";
+  console.log(scoreBoard);
+  // show final score
+  scoreTotal.textContent = "Your final score is :" + totalScore ;
+  // clearInterval(timerInterval);  
+  timeLeft.style.display = "none"; 
+};
+
+function userScore () {
+  var currentScore = localStorage.getItem("ScoreList");
+  if (currentScore !== null ){
+      newScoreList = JSON.parse(currentScore);
+      return newList;
+  } else {
+      newScoreList = [];
+  }
+  return newScoreList;
+};
+
 
 
 startBtn.addEventListener("click", startQuiz);
@@ -141,7 +168,7 @@ pickButtons.forEach(function(click){
 //save information and go to next page
 submitBtn.addEventListener("click", function(e) {
   e.preventDefault();
-  scoreBoard.style.display = "none";
+  scoreTally.style.display = "none";
   intro.style.display = "none";
   highScorePage.style.display = "block";
   questionsPage.style.display ="none";
